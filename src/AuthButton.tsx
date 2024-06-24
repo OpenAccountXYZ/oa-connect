492ff8044b66505113a83e6e70e6ebb3ea42eaa7
suneal <xurigoong@gmail.com>
2024-06-24 20:18:13 +0800

    update style

--- src/AuthButton.tsx
+++ src/AuthButton.tsx
@@ -36,25 +36,24 @@ const AuthButton: React.FC<AuthButtonProps> = ({ challenge, onClick ,default_url
     <div
       onClick={() => openAuthWindow(challenge, default_url)}
       style={{
-        width: '500px',
-        height: "72px",
+        width: '300px',
+        height: "50px",
         borderRadius: '0.5rem',
       }}
 
-      className="flex cursor-pointer justify-center items-center px-3 py-2.5 text-center rounded-lg border border-solid border-zinc-300 max-md:px-5">
+      className="flex cursor-pointer justify-center items-center px-3 py-2.5 text-center rounded-lg border border-solid border-zinc-300 max-md:px-5 hover:bg-gray-500 hover:duration-200 border-2">
       <div className="flex gap-3 items-center">
         <img
           loading="lazy"
           style={{
-            width: '48px',
-            height: '48px',
+            width: '28px',
+            height: '28px',
           }}
           src="https://account.test.oa.xyz/_next/image?url=%2Ficon.png&w=64&q=75"
           className="shrink-0 aspect-square "
         />
         <div className="my-auto" style={{
-            fontSize: '24px',
-            fontWeight: '500'
+            fontSize: '18px'
           }}>Sign in with OpenAccount</div>
       </div>
     </div>
